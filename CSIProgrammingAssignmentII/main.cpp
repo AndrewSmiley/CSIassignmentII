cfd94044290e4519a13e1ab7f6fa40a28a64cb29
Andrew Smiley <andrew.smiley@live.com>
2014-05-28 19:08:42 -0400

    Initial Commit

---
+++ CSIProgrammingAssignmentII/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CSIProgrammingAssignmentII
+//
+//  Created by Andrew Smiley on 5/28/14.
+//  Copyright (c) 2014 Andrew Smiley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
